50bb8a03ec3de459b6f35c2feebc05e384a35bed
diegargon <diego@envigo.net>
2024-12-28 14:24:31 +0100

    basic host info

--- src/info_linux.py
+++ src/info_linux.py
@@ -4,14 +4,12 @@ import socket
 
 def get_load_avg():
     load1, load5, load15 = os.getloadavg()
-    current_cpu_usage = cpu_usage(load1)
-    
+
     return {
         "loadavg": {
             "1min": round(load1, 2),
             "5min": round(load5, 2),
-            "15min": round(load15, 2),
-            "usage": round(current_cpu_usage, 2)
+            "15min": round(load15, 2)
         }
     }
 
@@ -46,60 +44,3 @@ def get_hostname():
 
 def get_ip_address(hostname):
     return socket.gethostbyname(hostname)
-
-def get_disks_info():
-    """
-    Obtain disks info from /proc/mount (/dev)
-
-    Returns:
-        dict: Disk partions info Key: disks
-    """
-    disks_info = []
-
-    # Read
-    with open("/proc/mounts", "r") as mounts:
-        for line in mounts:
-            parts = line.split()
-            device, mountpoint, fstype = parts[0], parts[1], parts[2]
-
-            # Filter
-            if not device.startswith("/dev/") or device == '/dev/fuse':
-                continue
-
-            # Info os.statvfs
-            try:
-                stat = os.statvfs(mountpoint)
-                total = bytes_to_mb(stat.f_blocks * stat.f_frsize)
-                free = bytes_to_mb(stat.f_bfree * stat.f_frsize)
-                used = total - free
-                percent = (used / total) * 100 if total > 0 else 0
-                disks_info.append({
-                    "device": device,           # Nombre del dispositivo
-                    "mountpoint": mountpoint,   # Punto de montaje
-                    "fstype": fstype,           # Tipo de sistema de archivos
-                    "total": total,             # Tamaño total en bytes
-                    "used": used,               # Espacio usado en bytes
-                    "free": free,               # Espacio libre en bytes
-                    "percent": round(percent, 2)# Porcentaje usado
-                })
-            except OSError:
-                continue
-
-    return {"disksinfo": disks_info}
-
-def get_cpus():
-    return os.cpu_count()
-
-def get_uptime():
-    with open('/proc/uptime', 'r') as f:
-        uptime_seconds = float(f.readline().split()[0])
-    return uptime_seconds
-
-def cpu_usage(cpu_load):
-    total_cpus = get_cpus()
-    if total_cpus == 0:
-        return False
-    
-    cpu_usage_percentage = (cpu_load / total_cpus) * 100
-
-    return round(cpu_usage_percentage, 2)    
