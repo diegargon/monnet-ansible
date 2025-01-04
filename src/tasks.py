ad38912aa47a21742687d426230ce72231c64d25
diegargon <diego@envigo.net>
2025-01-04 02:04:54 +0100

    ports

--- src/tasks.py
+++ src/tasks.py
@@ -4,23 +4,23 @@ import threading
 import globals
 import info_linux
 from log_linux import log, logpo
+import utils
 
-
-def check_listen_ports(datastore, notify_callback):
+def check_ports(datastore, notify_callback):
     
     if 'check_ports' in globals.timers:
         globals.timers['check_ports'].cancel()
     
-    current_listen_ports_info = info_linux.get_listen_ports_info()
-    last_listen_ports_info = datastore.get_data("last_listen_ports_info")
-    
-    if (current_listen_ports_info != last_listen_ports_info):        
-        datastore.update_data("last_listen_ports_info", current_listen_ports_info)
-        notify_callback("listen_ports_info", current_listen_ports_info)  # Notificar
-    #else : #debug
-    #    notify_callback("listen_ports_info", current_listen_ports_info)  # Notificar
+    """ defaultdict need normalization for compare """        
+    current_ports_info = utils.normalize(info_linux.get_ports_grouped())
+    last_ports_info = utils.normalize(datastore.get_data("last_ports_info"))
 
-    globals.timers['check_ports']  = threading.Timer(15, check_listen_ports, args=(datastore, notify_callback))
+    #if not utils.are_equal(current_ports_info, last_ports_info):
+    if (current_ports_info != last_ports_info):        
+        datastore.update_data("last_ports_info", current_ports_info)
+        notify_callback("ports_info", current_ports_info)  # Notificar al __main__       
+
+    globals.timers['check_ports']  = threading.Timer(15 * 60, check_ports, args=(datastore, notify_callback))
     globals.timers['check_ports'].start()
         
     #return timer  # Retorna el objeto Timer para control
