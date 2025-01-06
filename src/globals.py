c938e13948326310603095a875be5c5192f22374
diegargon <diego@envigo.net>
2025-01-06 02:07:32 +0100

    alerts works, fixes

--- src/globals.py
+++ src/globals.py
@@ -2,3 +2,20 @@
 
 # Track timers
 timers = {}
+
+# Constants
+
+# Log Types
+
+LT_DEFAULT = 0
+LT_EVENT = 1
+LT_REMOTE_PORT_STATUS = 2
+LT_ALERT = 3
+LT_WARN = 4
+LT_EVENT_ALERT = 5
+LT_EVENT_WARN = 6
+
+# Threshold
+
+ALERT_THRESHOLD = 90
+WARN_THRESHOLD = 80
